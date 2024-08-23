0bae62413272976834ca117b517fb45affcbb586
Sadia7725 <sadiasafdar7725@gmail.com>
2024-08-23 16:03:36 +0500

    ++

--- src/Pages/ContactUsPage.js
+++ src/Pages/ContactUsPage.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ContactCard from '../Components/Contact/ContactCard';
-import ContactInfo from '../Components/Contact/ContactInfo';
-import { contactItems } from '../data/contactData'; // Ensure the path is correct
+
 
 const ContactUsPage = () => {
   return (
     <div>
       <ContactCard />
-      {/* Pass contactItems as a prop to ContactInfo */}
-      <ContactInfo contactItems={contactItems} />
+    
     </div>
   );
 };
